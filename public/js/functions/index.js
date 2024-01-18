8e285f812e48a052328ac3d6e576467b0a407563
patan1213 <patanasif616@gmail.com>
2024-01-19 00:13:49 +0530

    message

---
+++ public/js/functions/index.js
@@ -0,0 +1,16 @@
+function sum(a,b)
+{
+    console.log(a+b)
+}
+console.log(sum)
+function mylogic (fun1)
+{
+    fun1(5,10)
+}
+mylogic(sum)
+const raj=function()
+{
+    console.log(hi,everyone)
+    return"bye"
+}
+console.log(raj)
